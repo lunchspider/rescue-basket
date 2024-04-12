e39913c37e5bba4e6b1a367a6794ca7f4d2ad9a5
lunchspider <lunchspider03@gmail.com>
2024-04-12 13:20:39 +0530

    added font

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <main className="h-screen bg-black"></main>;
+  return <main className="h-screen bg-black">
+    <div>Test</div>
+  </main>;
 }
